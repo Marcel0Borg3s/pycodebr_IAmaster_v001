f5f8977d5a0bde9865c542ac9f83eb1db9a9b4db
Marcelo Borges <marcelosbgs@gmail.com>
2025-06-03 08:50:44 -0300

    atualiza do RAG

--- mod04/mod05/RAG_1.py
+++ mod04/mod05/RAG_1.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
+from langchain import hub
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 from langchain_chroma import Chroma
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -14,7 +17,7 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 # Modelo
 model = ChatOpenAI(model_name='gpt-4', openai_api_key=OPENAI_API_KEY)
 
-pdf_path = 'Manual Acer Aspire 5'
+pdf_path = 'manual_fe16.pdf'
 loader = PyPDFLoader(pdf_path)
 
 docs = loader.load()
@@ -22,7 +25,7 @@ docs = loader.load()
 # Quebrar em chunks > from langchain_text_splitters
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000, #aqui é o tamanho da quebra em tokens
-    chunck_overlap=200, #e aqui já é o tamanho do overlap 
+    chunk_overlap=200, #e aqui já é o tamanho do overlap 
 )
 chunks = text_splitter.split_documents(
     documents=docs,
@@ -39,8 +42,22 @@ vector_store = Chroma.from_documents(
 
 retriever = vector_store.as_retriever()
 
-result = retriever.invoke(
-    'Qual sistema operacional vem instalado?'
+prompt = hub.pull('rlm/rag-prompt')
+
+rag_chain = (
+    {
+        'context': retriever, # aqui é o retorno dos dados do Vector Store
+        'question': RunnablePassthrough(),
+    }
+    | prompt
+    | model
+    | StrOutputParser()
 )
-print(result)
+
+
+
+question = 'como restaurar o computador?'
+
+response = rag_chain.invoke(question)
+print(response)
 
